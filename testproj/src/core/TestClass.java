93ab42686797f13eb7d79e0beae868cbe43f2ef1
pvmpa <pvmpa@iscte-iul.pt>
2017-10-04 12:12:05 +0100

    Added personality to TestClass

--- testproj/src/core/TestClass.java
+++ testproj/src/core/TestClass.java
@@ -1,5 +1,20 @@
 package core;
 
-public class TestClass {
+import java.util.ArrayList;
 
+public class TestClass {
+	private double stupidity;
+	private ArrayList<String> hopes_and_dreams = new ArrayList<String>();
+	
+	public TestClass(){
+		stupidity = 100;
+		hopes_and_dreams.add("Be a good object");
+	}
+	
+	public TestClass(double arg0){
+		stupidity = arg0;
+		hopes_and_dreams.add("Be as stupid as the user tells him to be");
+	}
+	
+	
 }
